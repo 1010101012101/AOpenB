822e6c801f5a6eddfcc1e4627febe1813d988ed0
Joe Lv <joel@wicresoft.com>
2018-04-19 15:37:41 +0800

    A open B

---
+++ ApplicationA/ApplicationA/MainActivity.cs
@@ -0,0 +1,49 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+using Android.Runtime;
+
+namespace ApplicationA
+{
+    [Activity(Label = "ApplicationA", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            Intent intent = new Intent();
+            intent.SetClassName("ApplicationB.ApplicationB", "ApplicationB.ApplicationB.MainActivity");
+            intent.PutExtra("Activity", "BundleActivity");
+            intent.PutExtra("Operation", "Task_1");
+            StartActivityForResult(intent,1);
+           
+        }
+
+        protected override void OnActivityResult(int requestCode, [GeneratedEnum] Result resultCode, Intent data)
+        {
+            if (resultCode == Result.Ok)
+            {
+                Toast.MakeText(this, " Result.Ok", ToastLength.Short).Show();
+                switch (requestCode)
+                {
+                    case 1:
+                        {
+                            if (data != null)
+                            {
+                                string dataReturned = data.GetStringExtra("AppBData");
+                                Toast.MakeText(this, dataReturned, ToastLength.Short).Show();
+                            }
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
+
---
+++ ApplicationB/ApplicationB/MainActivity.cs
@@ -0,0 +1,41 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+
+namespace ApplicationB
+{
+    [Activity(Label = "ApplicationB", MainLauncher = true,Name = "ApplicationB.ApplicationB.MainActivity")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            if (Intent != null)
+            {
+                string operation = Intent.GetStringExtra("Operation");
+                string activity = Intent.GetStringExtra("Activity");
+                if ("Task_1".Equals(operation))
+                {
+                    Toast.MakeText(this, operation+"------"+ activity, ToastLength.Short).Show();
+                }
+            }
+            Button bt = FindViewById<Button>(Resource.Id.bt);
+            bt.Click += Bt_Click;
+
+        }
+
+        private void Bt_Click(object sender, System.EventArgs e)
+        {
+            var returnIntent = new Intent();
+            returnIntent.PutExtra("AppBData", "AppBData");
+            SetResult(Result.Ok, returnIntent);
+            Finish();
+        }
+    }
+}
+
